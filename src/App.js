d170f0efbc78ee780e11858cf59896571999fea7
Harshvardhan Sawal <hv.sawal@gmail.com>
2020-11-20 13:07:07 -0500

    slight sidebar adjustment and flex div created for feed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import Sidebar from './Sidebar';
+import Feed from './Feed';
 import './App.css';
 
 function App() {
   return (
     // BEM convention
     <div className="app">
-      <h1>First things first!</h1>
+      {/* <h1>First things first!</h1> */}
 
       {/* Sidebar */}
       <Sidebar />
 
       {/* Feed */}
-      
+      <Feed />
+
       {/* Widget */}
     </div>
   );
---
+++ src/Feed.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Feed() {
+    return (
+        <div>
+           {/* Header */}
+           <h2>Home</h2>
+
+           {/* TweetBox */}
+
+           {/* Posts */}
+
+        </div>
+    )
+}
+
+export default Feed
